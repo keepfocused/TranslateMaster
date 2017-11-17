608f814a01934c4c6caff66f7b7246c1effe009b
Danil <keepfocused@users.noreply.github.com>
2017-11-17 23:13:59 +0300

    Google API SignIn added

--- TranslateMaster/FirstViewController.swift
+++ TranslateMaster/FirstViewController.swift
@@ -6,21 +6,100 @@
 //  Copyright © 2017 Danil. All rights reserved.
 //
 
+import GoogleAPIClientForREST
+import GoogleSignIn
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
+    
+    
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLRAuthScopeGmailReadonly]
+    
+    private let service = GTLRGmailService()
+    let signInButton = GIDSignInButton()
+    let output = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Configure Google Sign-in.
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // Add the sign-in button.
+        view.addSubview(signInButton)
+        
+        // Add a UITextView to display output.
+        output.frame = view.bounds
+        output.isEditable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        output.isHidden = true
+        view.addSubview(output);
     }
-
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            self.service.authorizer = nil
+        } else {
+            self.signInButton.isHidden = true
+            self.output.isHidden = false
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            fetchLabels()
+        }
     }
-
-
+    
+    // Construct a query and get a list of upcoming labels from the gmail API
+    func fetchLabels() {
+        output.text = "Getting labels..."
+        
+        let query = GTLRGmailQuery_UsersLabelsList.query(withUserId: "me")
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:))
+        )
+    }
+    
+    // Display the labels in the UITextView
+    func displayResultWithTicket(ticket : GTLRServiceTicket,
+                                 finishedWithObject labelsResponse : GTLRGmail_ListLabelsResponse,
+                                 error : NSError?) {
+        if let error = error {
+            showAlert(title: "Error", message: error.localizedDescription)
+            return
+        }
+        var labelString = ""
+        if let labels = labelsResponse.labels, labels.count > 0 {
+            labelString += "Labels:\n"
+            for label in labels {
+                labelString += "\(label.name!)\n"
+            }
+        } else {
+            labelString = "No labels found."
+        }
+        output.text = labelString
+    }
+    
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
 }
-
