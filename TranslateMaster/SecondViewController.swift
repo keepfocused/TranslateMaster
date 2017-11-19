e2772e07764c1f41ceca2f392af2e37d271277a2
Danil <keepfocused@users.noreply.github.com>
2017-11-19 23:57:24 +0300

    Google api

--- TranslateMaster/SecondViewController.swift
+++ TranslateMaster/SecondViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var mailSearchService = GoogleAuthViewController()
+        
+        mailSearchService 
+        
+        
+        
+
     }
 
 
